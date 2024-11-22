8630a82871607926720831f750f0aac818cb1871
Kieran F <kieran.finger@gmail.com>
2024-11-21 19:01:15 -0600

    add kieran as participant

--- participants.ts
+++ participants.ts
@@ -1,6 +1,7 @@
 const participants = [
   { firstName: 'Victor', lastName: 'Hofstetter' },
   { firstName: 'Akshat', lastName: 'Ghoshal' },
+  { firstName: 'Kieran', lastName: "Finger" }
 ];
 
 export default participants;
